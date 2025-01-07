527ada4fadee8d68b87ba13f85bf70cd1fe7e34d
AmeerDraz <ameerabudraz1@gmail.com>
2025-01-07 11:24:07 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const inputField = document.querySelector('.massage-input');
+
+// inputField.addEventListener('input', () => {
+//   inputField.style.height = 'auto'; // إعادة تعيين الارتفاع
+//   inputField.style.height = `${inputField.scrollHeight}px`; // ضبط الارتفاع بناءً على المحتوى
+// });
